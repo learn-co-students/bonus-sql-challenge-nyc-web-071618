667f9740f7950dc396482f0ff73ac6d3c74cb55f
chejianchao <820992376@qq.com>
2018-07-29 15:19:17 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,8 @@
 require 'bundler'
+require 'csv'
+require './lib/queries'
 Bundler.require
 
 # Setup a DB connection here
+#DB={conn: SQLite3::Database.new( filename ) }
+DB = SQLite3::Database.new('db/daily_show_guests.db')
